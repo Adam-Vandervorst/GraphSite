3bde52fd6c525275619426e93ccff5a06180ae6c
Adam-Vandervorst <adam.vandervorst@gmail.com>
2021-05-25 03:40:01 +0200

    Generate objects json from an actual graph.

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import flask
 from flask import Flask
 
+from HEdit.utils import json, HDict
+
+
+site_graph = HDict.load_from_path("site_graph.json")
+page_objects = {}
+for page_node in site_graph.as_objects():
+    del page_node['id']
+    name = page_node.pop('data')
+    page_objects[name] = page_node
+
+with open("./static/generated_objects.json", 'w') as f:
+    json.dump(page_objects, f)
+
+
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 

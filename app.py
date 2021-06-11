6d5aea970cc5dc212d6ca84ef1fb54ffb815f0f0
Adam-Vandervorst <adam.vandervorst@gmail.com>
2021-06-11 14:48:10 +0200

    Lay out current fields for parametric generation.

--- app.py
+++ app.py
@@ -21,29 +21,43 @@ def pages_view(page_id):
     return flask.render_template(f"/pages/{adjacency[page_id]['data']}.html", **adjacency[page_id], pages=adjacency)
 
 
-@app.route('/timeline', defaults={'date': '2021'})
-@app.route('/timeline/<date>')
-def timeline_view(date):
-    date_page_ids = defaultdict(list)
-    for page in adjacency.values():
-        date_page_ids[page['date']].append(page['id'])
-    if date not in date_page_ids:
-        return flask.redirect("/timeline")
-    from_page_id = flask.request.args.get('from_id', None, int)
-    return flask.render_template("timeline.html", chosen_date=date, date_page_ids=date_page_ids, from_page_id=from_page_id, pages=adjacency)
+date_page_ids = defaultdict(list)
+for page in adjacency.values():
+    date_page_ids[page['date']].append(page['id'])
+
+field_type_context = dict(
+    field_name="date",
+    field_page_ids=date_page_ids
+)
 
 
-@app.route('/labels', defaults={'label': 'highlight'})
-@app.route('/labels/<label>')
-def labels_view(label):
-    labels_page_ids = defaultdict(list)
-    for page in adjacency.values():
-        for page_label in page['labels']:
-            labels_page_ids[page_label].append(page['id'])
-    if label not in labels_page_ids:
-        return flask.redirect("/labels")
-    from_page_id = flask.request.args.get('from_id', None, int)
-    return flask.render_template("labels.html", chosen_label=label, labels_page_ids=labels_page_ids, from_page_id=from_page_id, pages=adjacency)
+@app.route('/date')
+def date_view():
+    overview_context = dict(
+        from_page_id=flask.request.args.get('from_id', None, int),
+        filters=flask.request.args.getlist('filter')
+    )
+    return flask.render_template("one_to_many.html", **overview_context, **field_type_context, pages=adjacency)
+
+
+labels_page_ids = defaultdict(list)
+for page in adjacency.values():
+    for page_label in page['labels']:
+        labels_page_ids[page_label].append(page['id'])
+
+field_type_context = dict(
+    field_name="labels",
+    field_page_ids=labels_page_ids
+)
+
+
+@app.route('/labels')
+def labels_view():
+    overview_context = dict(
+        from_page_id=flask.request.args.get('from_id', None, int),
+        filters=flask.request.args.getlist('filter')
+    )
+    return flask.render_template("one_to_many.html", **overview_context, **field_type_context, pages=adjacency)
 
 
 if __name__ == '__main__':

8602013941b87332326c1fdf64cc24e69c4fe689
Adam-Vandervorst <adam.vandervorst@gmail.com>
2021-06-14 15:57:06 +0200

    Implement partition and labeling logics.

--- app.py
+++ app.py
@@ -8,8 +8,23 @@ from HEdit.utils import json, HDict
 graph = HDict.load_from_path("site_graph.json")
 adjacency = graph.as_object_adjacency(*graph.split_node_types())
 
-with open("./static/generated_objects.json", 'w') as f:
-    json.dump(adjacency, f)
+with open("./static/generated_objects.json", 'w') as w_file:
+    json.dump(adjacency, w_file)
+
+
+def partition_context(field_name):
+    field_page_ids = defaultdict(list)
+    for page in adjacency.values():
+        field_page_ids[page[field_name]].append(page['id'])
+
+    return dict(field_type="partition", field_name=field_name, field_page_ids=field_page_ids, fields=field_page_ids.keys())
+
+
+def label_context(field_name):
+    page_id_fields = {page['id']: page[field_name] for page in adjacency.values()}
+    fields = {f for fs in page_id_fields.values() for f in fs}
+
+    return dict(field_type="label", field_name=field_name, page_id_fields=page_id_fields, fields=fields)
 
 
 app = flask.Flask(__name__, static_url_path='', static_folder='static')
@@ -21,43 +36,24 @@ def pages_view(page_id):
     return flask.render_template(f"/pages/{adjacency[page_id]['data']}.html", **adjacency[page_id], pages=adjacency)
 
 
-date_page_ids = defaultdict(list)
-for page in adjacency.values():
-    date_page_ids[page['date']].append(page['id'])
-
-field_type_context = dict(
-    field_name="date",
-    field_page_ids=date_page_ids
-)
-
-
+date_context = partition_context('date')
 @app.route('/date')
 def date_view():
     overview_context = dict(
         from_page_id=flask.request.args.get('from_id', None, int),
         filters=flask.request.args.getlist('filter')
     )
-    return flask.render_template("one_to_many.html", **overview_context, **field_type_context, pages=adjacency)
+    return flask.render_template("partition.html", **date_context, **overview_context, pages=adjacency)
 
 
-labels_page_ids = defaultdict(list)
-for page in adjacency.values():
-    for page_label in page['labels']:
-        labels_page_ids[page_label].append(page['id'])
-
-field_type_context = dict(
-    field_name="labels",
-    field_page_ids=labels_page_ids
-)
-
-
+labels_context = label_context('labels')
 @app.route('/labels')
 def labels_view():
     overview_context = dict(
         from_page_id=flask.request.args.get('from_id', None, int),
         filters=flask.request.args.getlist('filter')
     )
-    return flask.render_template("one_to_many.html", **overview_context, **field_type_context, pages=adjacency)
+    return flask.render_template("label.html", **labels_context, **overview_context, pages=adjacency)
 
 
 if __name__ == '__main__':

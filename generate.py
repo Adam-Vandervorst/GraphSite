7fb6a10f745cb074c34cef03dba6b721767d6b2a
Adam-Vandervorst <adam.vandervorst@gmail.com>
2021-07-10 00:36:44 +0200

    Eliminate id's.

--- generate.py
+++ generate.py
@@ -8,7 +8,7 @@ class Generator:
     def __init__(self, pages_dir="pages", as_index=None, placeholder="placeholder.html"):
         self.as_index = as_index
         self.placeholder = placeholder
-        self.adjacency = {}
+        self.pages = []
         templates_dir = os.path.join(os.path.dirname(__file__), "templates")
         self.env = Environment(loader=FileSystemLoader([pages_dir, templates_dir]),
                                autoescape=False, trim_blocks=True, lstrip_blocks=True)
@@ -20,9 +20,9 @@ class Generator:
         graph = HDict.load_from_path(graph_file)
         tp1, tp2, tp3 = graph.node_types()
         struct = graph.synthesize_structure(tp1, tp2, tp3)
-        objects = graph.as_objects(tp1, tp2, tp3, struct)
+        struct.__lt__ = lambda x, y: x.data < y.data
         ins = cls(**kwargs)
-        ins.adjacency = {o.id: o for o in objects}
+        ins.pages = graph.as_objects(tp1, tp2, tp3, struct)
         return ins
 
     def url_for(self, endpoint, **params):
@@ -30,33 +30,33 @@ class Generator:
         return f"/{endpoint.replace(' ', '-')*(endpoint != self.as_index)}{'?'*bool(params)}{url_params}"
 
     def partitions_view(self, field_name):
-        field_page_ids = defaultdict(list)
-        for page in self.adjacency.values():
-            field_page_ids[getattr(page, field_name)].append(page.id)
+        field_pages = defaultdict(list)
+        for page in self.pages:
+            field_pages[getattr(page, field_name)].append(page)
 
         base = self.env.get_template(f"partition.html")
-        return base.render(pages=self.adjacency, url_for=self.url_for,
-                           field_type="partition", field_name=field_name, field_page_ids=field_page_ids, fields=field_page_ids.keys())
+        return base.render(url_for=self.url_for, field_type="partition",
+                           field_name=field_name, field_pages=field_pages.items(), fields=field_pages.keys())
 
     def labels_view(self, field_name):
-        page_id_fields = {page.id: getattr(page, field_name) for page in self.adjacency.values()}
-        fields = {f for fs in page_id_fields.values() for f in fs}
+        page_fields = [(page, getattr(page, field_name)) for page in self.pages]
+        fields = {f for _, fs in page_fields for f in fs}
 
         base = self.env.get_template(f"label.html")
-        return base.render(pages=self.adjacency, url_for=self.url_for,
-                           field_type="label", field_name=field_name, page_id_fields=page_id_fields, fields=fields)
+        return base.render(url_for=self.url_for, field_type="label",
+                           field_name=field_name, page_fields=page_fields, fields=fields)
 
-    def pages_view(self, page_id, link_fields, partition_fields, label_fields):
+    def pages_view(self, page, link_fields, partition_fields, label_fields):
         base = self.env.get_template("page.html")
-        return base.render(page=self.adjacency[page_id], pages=self.adjacency, url_for=self.url_for,
+        return base.render(page=page, url_for=self.url_for,
                            link_fields=link_fields, partition_fields=partition_fields, label_fields=label_fields,
-                           main=f"{self.adjacency[page_id].data}.html", placeholder=self.placeholder)
+                           main=f"{page.data}.html", placeholder=self.placeholder)
 
     def generate(self, out_path, links=(), partitions=(), labels=()):
-        for i, page in self.adjacency.items():
+        for page in self.pages:
             filename = f"{'index' if page.data == self.as_index else page.data.replace(' ', '-')}.html"
             with open(os.path.join(out_path, filename), 'w') as f:
-                f.write(self.pages_view(i, links, partitions, labels))
+                f.write(self.pages_view(page, links, partitions, labels))
 
         for partition_name in partitions:
             with open(os.path.join(out_path, f"{partition_name}.html"), 'w') as f:
